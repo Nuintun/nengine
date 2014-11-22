c3f23a41157082a54396ef4cbdb53025e5bdffb9
Nuintun <nuintun@qq.com>
2014-11-22 09:44:14 +0800

    update files

--- server.js
+++ server.js
@@ -5,61 +5,56 @@ var PORT = 8888,
     mime = require('mime'),
     path = require('path');
 
+function notFound(response){
+    var filepath = path.join(process.cwd(), 'Assets/404/404.html'),
+        stream = fs.createReadStream(filepath);
+
+    response.writeHead(404, {
+        'Content-Type': 'text/html'
+    });
+
+    stream.pipe(response);
+}
+
+function serverError(response){
+    response.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+
+    response.end(err);
+}
+
 var server = http.createServer(function (request, response){
     var pathname = url.parse(request.url).pathname,
-        realPath = path.join(__dirname, pathname);
+        realPath = path.join(process.cwd(), pathname);
 
     fs.exists(realPath, function (exists){
         if (!exists) {
-            fs.readFile('Assets/404/404.html', 'binary', function (err, file){
-                if (err) {
-                    response.writeHead(500, {
-                        'Content-Type': 'text/plain'
-                    });
-
-                    response.end(err);
-                } else {
-                    response.writeHead(404, {
-                        'Content-Type': 'text/html'
-                    });
-
-                    response.write(file, 'binary');
-                    response.end();
-                }
-            });
+            notFound(response);
         } else {
             fs.stat(realPath, function (err, stats){
                 if (err) {
-                    response.writeHead(500, {
-                        'Content-Type': 'text/plain'
-                    });
-
-                    response.end(err);
+                    serverError(response);
                 } else {
-                    var ext;
+                    var ext,
+                        stream,
+                        contentType;
 
-                    realPath = stats.isFile() ? realPath : realPath + 'index.html';
-                    ext = path.extname(realPath);
-                    ext = ext ? ext.slice(1) : 'unknown';
+                    if (stats.isFile()) {
+                        ext = path.extname(realPath);
+                        ext = ext ? ext.slice(1) : 'unknown';
+                        stream = fs.createReadStream(realPath);
+                        contentType = mime.lookup(ext) || 'text/plain';
 
-                    fs.readFile(realPath, 'binary', function (err, file){
-                        if (err) {
-                            response.writeHead(500, {
-                                'Content-Type': 'text/plain'
-                            });
+                        response.writeHead(200, {
+                            'Content-Type': contentType
+                        });
 
-                            response.end(err);
-                        } else {
-                            var contentType = mime.lookup(ext) || 'text/plain';
+                        stream.pipe(response);
+                    } else {
+                        notFound(response);
+                    }
 
-                            response.writeHead(200, {
-                                'Content-Type': contentType
-                            });
-
-                            response.write(file, 'binary');
-                            response.end();
-                        }
-                    });
                 }
             });
         }
